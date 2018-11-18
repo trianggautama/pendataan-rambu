f3356e420d8b421b73354fafaf6f0d9732fefd55
Tri Angga T.u <trianggategarutama@gmail.com>
2018-11-19 06:42:10 +0700

    ubah tampilan

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -7,28 +7,35 @@
     <!-- Main content -->
     <section class="content">
        <div class="box">
-           <div class="box-body" style="padding:30px; margin-top:0px;">
-             <div class="row">
-                    <div class="col-sm-12"  style="margin-bottom:10px;" >
-                          <img class=" img-responsive "  style="width:100%" src="/images/dashboard/banner.png"  >
-                     </div>      
-                 <div class="col-md-12">
-                        <div class="col-md-9 col-xs-12 text-center" style="padding:0px;">
-                                <img class=" img-responsive "  style="width:99%" src="/images/dashboard/data_rambu.png"  >
-                           </div>
-                           <div class="col-md-3 col-xs-6 " style="padding:0px; margin-left:0px;">
-                               <div class="col-md-12 col-xs-6" style="padding:0px; margin-bottom:10px; margin-left:10px;">
-                                    <img class=" img-responsive "  style="width:95%" src="/images/dashboard/kebutuhan_rambu.png"  >
-                               </div>
-                            <div class="col-md-12 col-xs-6" style="padding:0px;">
-                                    <img class=" img-responsive "  style="width:95% ; margin-left:10px;" src="/images/dashboard/rambu_terpasang.png"  >
-                            </div>
-                             
-                           </div>
-                 </div>
-                
-                       
-             </div>
+           <div class="box-body" style="margin-top:0px;" >
+            <div class="title" style="padding-left:52px;">   
+             <h2  style="margin-bottom:3px;"><b>Dinas</b> Perhubungan</h2>
+             <h4 style="margin-top:0px;">kota banjarbaru</h4>
+            </div>
+            </div>
+            <div class="row" style="padding:0px 50px 50px 50px">
+                <div class="col-md-8" style="padding:0px;">
+                    <div class="col-md-12">
+                      
+                        <img class=" img-responsive "  style="width:99%" src="/images/dashboard/data_rambu.png"  >
+                    </div>
+                    <div class="col-md-12">
+                        <div class="col-md-6" style="padding:0px; margin-top:1%; ">
+                            <img class=" img-responsive "  style="width:98%" src="/images/dashboard/rambu_terpasang.png"  >
+                        </div>
+                        <div class="col-md-6" style="padding:0px;  margin-top:1% ">
+                            <img class=" img-responsive "  style="width:98%" src="/images/dashboard/kebutuhan_rambu.png"  >
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4" style="padding:0px; margin:0px;">
+                   <div class="col-md-12" style="padding:0px; ">
+                    <img class=" img-responsive "  style="width:100%" src="/images/dashboard/lokasi.png"  >
+                   </div>
+                   <div class="col-md-12"  style="padding:0px; margin-top:20px;">
+                    <img class=" img-responsive "  style="width:100%" src="/images/dashboard/laporan.png"  >
+                </div>
+                </div>
             </div>
        </div>
     </section>
